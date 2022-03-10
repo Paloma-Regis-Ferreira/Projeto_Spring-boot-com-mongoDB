db4a987dc56b482410c4a0da8dd5d9767d2f3b0f
Paloma-Regis-Ferreira <palomaregis29@gmail.com>
2022-03-10 20:47:48 -0300

    limpando a base e mockando

---
+++ src/main/java/com/paloma/projetomongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.paloma.projetomongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.paloma.projetomongo.domain.User;
+import com.paloma.projetomongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll(); //vai zerar o mongo
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
