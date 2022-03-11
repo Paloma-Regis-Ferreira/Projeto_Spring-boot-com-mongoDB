39cf3b362e27018da9d56b39ab026edceb655474
Paloma-Regis-Ferreira <palomaregis29@gmail.com>
2022-03-10 21:53:16 -0300

    metodo post

--- src/main/java/com/paloma/projetomongo/resources/UserResource.java
+++ src/main/java/com/paloma/projetomongo/resources/UserResource.java
@@ -1,17 +1,19 @@
 package com.paloma.projetomongo.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.paloma.projetomongo.domain.User;
 import com.paloma.projetomongo.dto.UserDTO;
@@ -35,5 +37,13 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id){
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(user));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO){
+		User user = service.fromDTO(objDTO);
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/paloma/projetomongo/services/UserService.java
+++ src/main/java/com/paloma/projetomongo/services/UserService.java
@@ -1,12 +1,12 @@
 package com.paloma.projetomongo.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.paloma.projetomongo.domain.User;
+import com.paloma.projetomongo.dto.UserDTO;
 import com.paloma.projetomongo.repository.UserRepository;
 import com.paloma.projetomongo.services.exception.ObjectNotFoundException;
 
@@ -26,5 +26,13 @@ public class UserService {
 			throw new ObjectNotFoundException("Objeto nao encontrado");
 		}
 		return user;
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
