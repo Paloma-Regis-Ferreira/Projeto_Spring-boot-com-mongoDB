186136922729cd507074a02029232b09a36877b5
Paloma-Regis-Ferreira <palomaregis29@gmail.com>
2022-04-07 18:00:15 -0300

    author dto

--- src/main/java/com/paloma/projetomongo/config/Instantiation.java
+++ src/main/java/com/paloma/projetomongo/config/Instantiation.java
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.TimeZone;
 
+import com.paloma.projetomongo.dto.AuthorDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -30,15 +31,17 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.deleteAll(); //vai zerar o mongo
 		postRepository.deleteAll();
-		
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei", maria);
-		
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei", new AuthorDTO(maria));
+		
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
