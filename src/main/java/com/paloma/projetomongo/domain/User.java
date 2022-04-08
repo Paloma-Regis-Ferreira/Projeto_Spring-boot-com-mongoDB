31c731b31a92dc1a814c73d1defa2e7f2b34541e
Paloma-Regis-Ferreira <palomaregis29@gmail.com>
2022-04-08 17:58:41 -0300

    referenciando os posts em listas

--- src/main/java/com/paloma/projetomongo/domain/User.java
+++ src/main/java/com/paloma/projetomongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.paloma.projetomongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 //no mongodb isso indica que o dominio é a coleção
@@ -16,6 +19,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+
+	@DBRef(lazy = true) //nao vai retornar os posts somente se acessa-los
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 	}
@@ -49,6 +55,14 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
