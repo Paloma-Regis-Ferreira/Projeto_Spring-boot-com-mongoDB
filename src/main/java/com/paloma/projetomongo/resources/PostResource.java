0aca3a28b4c3fb0edade2b20ce4b1eefdb49a172
Paloma-Regis-Ferreira <palomaregis29@gmail.com>
2022-04-08 19:05:10 -0300

    adicionando filtro de busca por parametro com query methods do mongo

--- src/main/java/com/paloma/projetomongo/resources/PostResource.java
+++ src/main/java/com/paloma/projetomongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.paloma.projetomongo.resources;
 import com.paloma.projetomongo.domain.Post;
 import com.paloma.projetomongo.domain.User;
 import com.paloma.projetomongo.dto.UserDTO;
+import com.paloma.projetomongo.resources.util.URL;
 import com.paloma.projetomongo.services.PostService;
 import com.paloma.projetomongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.net.URLDecoder;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,5 +27,12 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post post = service.findById(id);
 		return ResponseEntity.ok().body(post);
+	}
+
+	@GetMapping(value = "/searchTitleBy")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 }
